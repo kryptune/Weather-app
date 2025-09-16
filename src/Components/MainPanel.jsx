7048a3ed38ff99007f9ade3424b10c81fa7cdaa3
kryptune <51198180+kryptune@users.noreply.github.com>
2025-09-16 22:49:43 +0800

    adjusted the font of the main panel

--- src/Components/MainPanel.jsx
+++ src/Components/MainPanel.jsx
@@ -4,7 +4,7 @@ export default function MainPanel ({city,data,capitalize}){
     const [rain ,setRain] = useState(0)
     const temperature = data?.main?.temp || "";
     const country = data?.sys?.country || "";
-    const textSize = city.length > 8 ? "text-2xl" : "text-3xl"
+    const textSize = city.length > 8 ? "text-xl" : "text-2xl"
     const currentDate = new Date();
     const options = {
     day: '2-digit',      // Two-digit day (e.g., 05)
@@ -19,10 +19,10 @@ export default function MainPanel ({city,data,capitalize}){
             setHour("3h")
             setRain(data?.rain?.["3h"])
         }
-        setRain(data?.rain?.[hour])
+        setRain(data?.rain?.[hour] || 0)
     }, [data] )
 
-    console.log( data?.rain?.["1h"] )
+
 
     return(
         <div className="flex rounded-3xl bg-[#242424] p-5 items-center justify-evenly md:px-7 md:py-10 md:h-[200px] md:mr-4">
@@ -30,10 +30,10 @@ export default function MainPanel ({city,data,capitalize}){
                 <h1 className="text-base text-[#D3D3D3] mb-1 md:text-xl">{formattedDate}</h1>
                 <h2 className={`${textSize} text-[#ffffff] font-bold md:text-5xl`}>{capitalize(city)}, {country} </h2>
             </div>
-            <div className="text-4xl text-[#ffffff] font-bold mr-4 md:text-6xl md:ml-4">{Math.round(temperature)}°C </div>
+            <div className="text-3xl text-[#ffffff] font-bold mr-4 md:text-6xl md:ml-4">{Math.round(temperature)}°C </div>
             <div className="flex flex-col justify-center p-2">
                 <p className="text-xl text-[#ffffff]">Rain({hour})</p>
-                <h3 className="text-2xl text-[#ffffff] font-bold">{rain} mm</h3>
+                <h3 className="text-xl text-[#ffffff] font-bold">{rain}mm</h3>
             </div>
         </div>
         
