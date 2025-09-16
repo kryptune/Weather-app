fb4141732c1d40af73e81342b56a76b15d009904
kryptune <51198180+kryptune@users.noreply.github.com>
2025-09-16 23:07:48 +0800

    fixed the centering of the description

--- src/Components/SidePanel.jsx
+++ src/Components/SidePanel.jsx
@@ -2,7 +2,6 @@ export default function SidePanel ({data, capitalize}) {
     const icon = data?.weather?.[0]?.icon || "01d";
     let weatherIcon = `https://openweathermap.org/img/wn/${icon}@2x.png`
     const description = data?.weather?.[0]?.description || "";
-    const spacing = description.length < 12 ? "left-2/5" : "left-1/3"
     const details = ["Sunrise","Sunset","Wind","Humidity" ]
     const sunriseDate = new Date((data?.sys?.sunrise + data?.timezone) * 1000);
     const sunsetDate = new Date((data?.sys?.sunset + data?.timezone) * 1000);
@@ -38,7 +37,7 @@ export default function SidePanel ({data, capitalize}) {
         <div className="md:flex  mb-9 md:mb-0">   
             <div className="relative flex flex-col w-full h-1/3 justify-center items-center mb-4 md:h-full md:order-1 md:ml-4 md:w-[200px]">
                 <img src={weatherIcon} className="md:absolute w-1/2 h-25 md:h-full md:w-[200px] md:top-[-10px] "/>
-                <div className={`absolute top-3/4 ${spacing} text-lg text-[#ffffff] md:left-[40px]`}>{capitalize(description)}</div>
+                <div className={`flex absolute justify-center items-center top-3/4 text-lg text-[#ffffff]`}>{capitalize(description)}</div>
             </div>
             <div className="flex rounded-3xl bg-[#242424] p-4 items-center justify-around md:inline-grid md:grid-cols-2 md:grid-rows-2 md:gap-4 md:min-w-[240px] md:px-7 md:py-10 md:h-[200px]">
                 {detailsArray}
